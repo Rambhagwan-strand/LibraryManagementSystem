bf612f7de800a383ed58b26c3dcc92c5110e511f
Rambhagwan-strand <85108239+Rambhagwan-strand@users.noreply.github.com>
2021-06-17 15:51:55 +0530

    Add files via upload

---
+++ LibraryManagementSystem/LibraryManagementSystem/Model/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Model
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public string Publication { get; set; }
+
+        public Book(string title, string author, string publication)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Publication = publication;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Book: {0}, Author: {1}, Publication: {2}", Title, Author, Publication);
+        }
+    }
+}
---
+++ LibraryManagementSystem/LibraryManagementSystem/ViewModel/LibraryManagementSystemVM.cs
@@ -0,0 +1,44 @@
+﻿using LibraryManagementSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    class LibraryManagementSystemVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Book> Books { get; set; }
+
+        
+
+
+        private string _sarchedText;
+
+        public string SarchedText
+        {
+            get { return _sarchedText; }
+            set { 
+                _sarchedText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public LibraryManagementSystemVM()
+        {
+            Books = new ObservableCollection<Book>();
+            Books.Add(new Book("A!", "SS", "VV"));
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
